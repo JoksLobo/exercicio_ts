a5080096112c9e34514a8a07bcc693557a8a5fae
Joks Lobo <joks@exemplo.com>
2024-08-04 07:24:52 -0300

    commit inicial

---
+++ src/atividade.js
@@ -0,0 +1,13 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+
+//resultados
+console.log(multiplicar(3, 4));
+console.log(saudacao("João"));
